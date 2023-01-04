c9dcd30f5ae6542c8a5ed1ea41768cac52fced03
Kyle2501 <kyle2501@gmail.com>
2023-01-04 07:01:23 -0700

    . added some Go Lang code

--- app/Go Software/_webapp/main.go
+++ app/Go Software/_webapp/main.go
@@ -1,5 +1,22 @@
-// . 
+// . _webapp Labs Foundation
+/// . . Ejected Media app for Linux UI
 
 
+package main
+
+// .
+
+import (
+
+	"os"
+	"log"
+	
+	"text/template"
+	"net/http"
+
+)
+
+
+// --
 
 
